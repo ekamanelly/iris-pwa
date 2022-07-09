3470254872e3dd17a8165c678c53891cca9c2951
ekamanelly <ekamanelly@gmail.com>
2022-07-09 01:46:00 +0100

    _

--- components/navigation/Header.tsx
+++ components/navigation/Header.tsx
@@ -6,7 +6,7 @@ export function Header({ children }: any) {
   return (
     <div className="h-[96px] w-full bg-black flex justify-between items-center pl-[40px] pr-[38px] ">
       <div className="w-[195px] h-[23px] lg:mr-[233px] md:mr-[70px] mr-[20px]">{logo}</div>
-      <div className="w-[657px] h-[45px] lg:mr-[41px] md:mr-[21px] mr-[20px] rounded-lg overflow-hidden">{searchField}</div>
+      <div className="hidden sm:block w-[657px] h-[45px] lg:mr-[41px] md:mr-[21px] mr-[20px] rounded-lg overflow-hidden ">{searchField}</div>
       <div className="md:hidden block text-white">menu</div>
       <div className="hidden md:block text-white">{navMenu}</div>
     </div>
